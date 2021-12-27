6f3b17b6560b41e85cbaf57f8ac205a8e9ea106c
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-12-27 20:41:07 +0100

    Changing few things necessary in order to get a correct response from API

--- app/src/main/java/com/example/currencyexchange/network/HistoricalRatesEndpoint.kt
+++ app/src/main/java/com/example/currencyexchange/network/HistoricalRatesEndpoint.kt
@@ -3,7 +3,7 @@ package com.example.currencyexchange.network
 
 import com.example.currencyexchange.BuildConfig
 import com.example.currencyexchange.model.HistoricalRatesResponse
-import retrofit2.Response
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,5 +13,5 @@ interface HistoricalRatesEndpoint {
     @GET("{date}")
     fun getHistoricalRates(@Path("date")date: String = "2021-12-27",
                            @Query("access_key") apiKey: String = BuildConfig.API_KEY)
-    : Response<HistoricalRatesResponse>
+    : Single<HistoricalRatesResponse>
 }
