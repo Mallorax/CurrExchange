8b2a70ec49f00958bbbeeaad589ecd857c664e6d
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-12-27 20:00:03 +0100

    Added basic layouts and navigation component

--- app/src/main/java/com/example/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchange/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.currencyexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.currencyexchange.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation(){
+        val appBarConfig = AppBarConfiguration(navController.graph)
+        setSupportActionBar(binding.appToolbar)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/example/currencyexchange/network/HistoricalRatesEndpoint.kt
+++ app/src/main/java/com/example/currencyexchange/network/HistoricalRatesEndpoint.kt
@@ -2,6 +2,8 @@ package com.example.currencyexchange.network
 
 
 import com.example.currencyexchange.BuildConfig
+import com.example.currencyexchange.model.HistoricalRatesResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,4 +13,5 @@ interface HistoricalRatesEndpoint {
     @GET("{date}")
     fun getHistoricalRates(@Path("date")date: String = "2021-12-27",
                            @Query("access_key") apiKey: String = BuildConfig.API_KEY)
+    : Response<HistoricalRatesResponse>
 }
