4416667619d6157a586c390cb0c8f3990c16ac22
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-12-29 22:33:31 +0100

    There has been a bug when date changes at midnight and API hasn't created an entry for a new date yet, the app will crash. Making a fix for that so that app will simply ignore failed response and procced to make requests for earlier days.

--- app/src/main/java/com/example/currencyexchange/model/DataMapers.kt
+++ app/src/main/java/com/example/currencyexchange/model/DataMapers.kt
@@ -5,12 +5,15 @@ import java.util.*
 
 fun mapToExchangeRatio(response: HistoricalRatesResponse): List<CurrencyExchangeModel>{
     val result = mutableListOf<CurrencyExchangeModel>()
+    if (response.success != true){
+        return result
+    }
     val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
     val cal = Calendar.getInstance()
     cal.time = dateFormat.parse(response.date)!!
     result.add(CurrencyExchangeDate(cal))
-    response.rates.forEach{(key, value) ->
-        result.add(CurrExchangeRatio(key, value, response.date, response.base))
+    response.rates?.forEach{(key, value) ->
+        result.add(CurrExchangeRatio(key, value, response.date.orEmpty(), response.base.orEmpty()))
     }
     return result
 }
