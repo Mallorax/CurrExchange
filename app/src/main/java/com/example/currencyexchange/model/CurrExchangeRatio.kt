8701c2d6348ecac7b132ede956fba0a1f25c8dc5
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-12-29 16:38:04 +0100

    Adjusted values of exchcange ratio to 5 decimal places for better readability

--- app/src/main/java/com/example/currencyexchange/model/CurrExchangeRatio.kt
+++ app/src/main/java/com/example/currencyexchange/model/CurrExchangeRatio.kt
@@ -3,4 +3,8 @@ package com.example.currencyexchange.model
 data class CurrExchangeRatio(
     val currency: String,
     val ratio: Double): CurrencyExchangeModel() {
+
+        fun getFormattedRatio():String{
+            return String.format("%.5f", ratio)
+        }
 }
